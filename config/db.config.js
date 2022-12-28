ff7362ece42d75da22543ab673029890d47a16b5
Radis Lamsuneekan <radisworldclassic.gm@gmail.com>
2022-12-28 16:33:14 +0700

    a

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123456789',
-    database: 'shop-songkhla',
-    port: '3307'
+    // host: 'localhost',
+    // user: 'root',
+    // password: '123456789',
+    // database: 'shop-songkhla',
+    // port: '3307'
+    host: 'sql.freedb.tech',
+    user: 'freedb_mynight',
+    password: 'TgM7d36*9J!5c$S',
+    database: 'freedb_db_mynight',
+    port: '3306',
 })
 
 connection.connect((err) => {
