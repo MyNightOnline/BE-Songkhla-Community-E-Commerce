073ca3032b37acd648c94e40bbc6c1fc23c7e373
Radis Lamsuneekan <radisworldclassic.gm@gmail.com>
2023-01-26 22:14:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-const express = require('express')
+const express = require("express")
 const cors = require("cors")
 const app = express()
 const PORT = 3001
 
-const router = require('./routes')
+const router = require("./routes")
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: false,
-}))
+  })
+)
 
-app.use('/test', (req,res) => {
-    res.send('hello world')
+app.use("/test", (req, res) => {
+  res.send("hello world")
 })
-app.use('/', router)
+app.use("/", router)
 
-app.listen(PORT, () => console.log('Server: http://localhost:3001'))
+app.listen(PORT, () => console.log("Server: http://localhost:3001"))
 
 module.exports = app
