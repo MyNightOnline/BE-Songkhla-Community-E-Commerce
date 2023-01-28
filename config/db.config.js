16ef52c39316b549219720f3096bf6b974085e0c
Radis Lamsuneekan <radisworldclassic.gm@gmail.com>
2023-01-28 18:20:55 +0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -6,10 +6,10 @@ const connection = mysql.createConnection({
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-  port: process.env.PORT,
-  // ssl: {
-  //   rejectUnauthorized: true,
-  // },
+  // port: process.env.PORT,
+  ssl: {
+    rejectUnauthorized: true,
+  },
 })
 
 connection.connect((err) => {
